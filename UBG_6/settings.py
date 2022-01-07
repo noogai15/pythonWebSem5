1a1d06260c2b94a41ba35e9484f5e2aa0512aee2
Yakup <ykohne@gmail.com>
2022-01-07 16:51:59 +0100

    MySQL settings

--- UBG_6/settings.py
+++ UBG_6/settings.py
@@ -79,8 +79,15 @@ WSGI_APPLICATION = "UBG_6.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "noogai15$yakupdeniz",
+        "USER": "noogai15",
+        "PASSWORD":"bky_yqe3mef!ZNJ2gyf",
+        "HOST":"noogai15.mysql.eu.pythonanywhere-services.com",
+        "OPTIONS": {
+            'init_command': "SET sql_mode='STRICT_ALL_TABLES'",
+            'charset': 'utf8'
+        }
     }
 }
 
