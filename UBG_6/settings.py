e784a8063415b99863c53f61c987a46a04b3bce1
Yakup <ykohne@gmail.com>
2022-02-06 16:38:24 +0100

    .

--- UBG_6/settings.py
+++ UBG_6/settings.py
@@ -145,6 +145,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+LOGIN_URL = ""
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
